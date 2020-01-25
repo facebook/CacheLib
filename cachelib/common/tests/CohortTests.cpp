7a8e148aa049bc89ab6fdc83c1082fc9b9e05516
Sathya Gunasekar <sathya@fb.com>
2020-01-24 16:37:03 -0800

    add multi-threaded test for cohort
    Reviewed By: davidgoode
    
    Differential Revision: D19560333
    
    fbshipit-source-id: a8a2db6ea870b1d0588eccc19963bb472e872b39

--- cachelib/common/tests/CohortTests.cpp
+++ cachelib/common/tests/CohortTests.cpp
@@ -1,5 +1,6 @@
 #include "cachelib/common/Cohort.h"
 
+#include <folly/Random.h>
 #include <folly/synchronization/Baton.h>
 #include <gtest/gtest.h>
 
@@ -68,6 +69,54 @@ TEST(Cohort, MaintainTopOrBottom) {
   EXPECT_NE(isTop, cohort.isTopCohort());
   EXPECT_NE(isTop, cohort.incrActiveReqs());
   EXPECT_NE(isTop, cohort.incrActiveReqs());
+}
+
+// ensure that incrementing cohort is consistent with switching in a
+// multi-threaded env.
+TEST(Cohort, MultiThreaded) {
+  Cohort cohort;
+  int nBumps = folly::Random::rand32(1000, 2000);
+  int nSwitches = nBumps;
+
+  // we start with bottom cohort
+  EXPECT_FALSE(cohort.isTopCohort());
+
+  auto incrDecrInThread = [&]() {
+    for (int i = 0; i < nBumps; i++) {
+      auto isTop = cohort.incrActiveReqs();
+      std::this_thread::sleep_for(std::chrono::microseconds(10));
+      cohort.decrActiveReqs(isTop);
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 32; i++) {
+    threads.emplace_back(incrDecrInThread);
+  }
+
+  threads.emplace_back([&]() {
+    for (int i = 0; i < nSwitches; i++) {
+      cohort.switchCohorts();
+      std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+  });
+
+  for (auto& t : threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+
+  EXPECT_EQ(cohort.getPending(cohort.isTopCohort()), 0ULL);
+  EXPECT_EQ(cohort.getPending(!cohort.isTopCohort()), 0ULL);
+
+  // ensure the cohort state matches the number of times we could have
+  // switched.
+  if (nSwitches % 2 == 0) {
+    EXPECT_FALSE(cohort.isTopCohort());
+  } else {
+    EXPECT_TRUE(cohort.isTopCohort());
+  }
 }
 
 } // namespace tests
