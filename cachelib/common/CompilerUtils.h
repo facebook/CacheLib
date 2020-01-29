a5bddc9840d13162b2aafdcb20f37656f43793c5
Eden Zik <ezik@fb.com>
2020-01-29 14:01:31 -0800

    Eliminate cachelib namespace pollution of "detail::" by macro
    Summary: This macro is used in Zinc to ensure standard layout of fields. However, the classes it is used are in a global "detail::" namespace which conflicts with others.
    
    Reviewed By: sathyaphoenix
    
    Differential Revision: D19594580
    
    fbshipit-source-id: 4a0423786a564ad804a6628bdce94fa76252c306

--- cachelib/common/CompilerUtils.h
+++ cachelib/common/CompilerUtils.h
@@ -3,6 +3,9 @@
 #define CACHELIB_PACKED_ATTR __attribute__((__packed__))
 #define CACHELIB_INLINE __attribute__((__always_inline__))
 
+namespace facebook {
+namespace cachelib {
+
 /**
  * If the value is set to true for some type T, then T is safe to persistently
  * store in shared memory or dipper.
@@ -21,18 +24,16 @@ struct CheckSize {};
  * Certifies that the type named by 'Name' is of the given size and is
  * safe to store in shared memory or dipper.
  */
-#define CACHELIB_SHM_CERTIFY(Name, size)                                       \
-  template <>                                                                  \
-  struct IsShmSafe<Name> {                                                     \
-    static constexpr bool value = true;                                        \
-  };                                                                           \
-  constexpr detail::CheckSize<sizeof(Name)> FB_ANONYMOUS_VARIABLE(checkSize) = \
-      detail::CheckSize<size>();                                               \
-  static_assert(std::is_standard_layout<Name>::value,                          \
+#define CACHELIB_SHM_CERTIFY(Name, size)                        \
+  template <>                                                   \
+  struct facebook::cachelib::IsShmSafe<Name> {                  \
+    static constexpr bool value = true;                         \
+  };                                                            \
+  constexpr facebook::cachelib::detail::CheckSize<sizeof(Name)> \
+      FB_ANONYMOUS_VARIABLE(checkSize) =                        \
+          facebook::cachelib::detail::CheckSize<size>();        \
+  static_assert(std::is_standard_layout<Name>::value,           \
                 #Name "must be standard layout")
-
-namespace facebook {
-namespace cachelib {
 
 // convenience struct for getting the number of bits in a byte.
 template <typename T>
