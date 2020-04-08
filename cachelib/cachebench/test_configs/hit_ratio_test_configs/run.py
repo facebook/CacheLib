c67452c41a4ec5a97a9a7207b023c37fb0842bd9
Daniel S. Berger <dsberger@cmu.edu>
2020-04-08 14:40:56 +0000

    improved throughput run script

--- cachelib/cachebench/test_configs/hit_ratio_test_configs/run.py
+++ cachelib/cachebench/test_configs/hit_ratio_test_configs/run.py
@@ -27,7 +27,7 @@ config = {
         0.7
       ], 
       "loneGetRatio": 0.025391725016963105, 
-      "numKeys": 23458726, 
+      "numKeys": 23458726,
       "numOps": 5000000, 
       "numThreads": 1,
       "popDistFile": "assoc_altoona1_follower_pop.json", 
@@ -40,30 +40,50 @@ config = {
 
 outfile = open('run.csv', 'w')
 writer = csv.writer(outfile)
-for threads in [1,4,8,16]:
-    config["test_config"]["numThreads"] = threads
-    config["test_config"]["numKeys"] = 100000
-    config["test_config"]["numOps"] = 1000
-    
-    
-    with open("run.json","w") as jfile:
-        json.dump(config, jfile, indent=2)
-    
-    #sudo /home/daniel/cachelib/build/bin/cachebench_bin --json_test_config ./fbobj_altoona2_follower_model_hr.json
-    
-    p = subprocess.Popen(["sudo","/home/daniel/cachelib/build/bin/cachebench_bin","--json_test_config","./run.json"], stdout=subprocess.PIPE)
-    
-    
-    exp = "[0-9,]+(?=/)"
-    
-    res = [config["test_config"]["numThreads"],
-        config["test_config"]["numKeys"],
-        config["test_config"]["numOps"]]
 
-    for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
-        match = re.search(exp, line)
-        if match:
-            res.append(int(match.group().replace(',', '')))
-    print(res)
-    writer.writerow(res)
+
+# need repetitions
+# different cache configs
+# different working set sizes
+
+for rep in range(0,10):
+    for sys in ["assoc","fbobj"]:
+        for num_keys in [2345872,23458726,234587260]:
+            for threads in [1,2,4,8,16]:
+                config["test_config"]["popDistFile"] = sys+"_altoona1_follower_pop.json"
+                config["test_config"]["valSizeDistFile"] = sys+"_altoona1_follower_sizes.json"
+                config["test_config"]["numThreads"] = threads
+                config["test_config"]["numKeys"] = num_keys
+                config["test_config"]["numOps"] = 300000000
+
+                res = [
+                    sys,
+                    config["test_config"]["numThreads"],
+                    config["test_config"]["numKeys"],
+                    config["test_config"]["numOps"],
+                    rep
+                ]
+                
+                with open("run.json","w") as jfile:
+                    json.dump(config, jfile, indent=2)
+                
+                #sudo /home/daniel/cachelib/build/bin/cachebench_bin --json_test_config ./fbobj_altoona2_follower_model_hr.json
+                
+                p = subprocess.Popen(["sudo","/home/daniel/cachelib/build/bin/cachebench_bin","--json_test_config","./run.json"], stdout=subprocess.PIPE)
+            
+                warmup = True
+                for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+                    if "Test Results" in line:
+                        warmup = False
+                    if not warmup:
+                        if "Hit Ratio" in line:
+                            match = re.search("[0-9.]+(?=%)", line)
+                            if match:
+                                res.append(float(match.group().replace(',', '')))
+                        if re.search("get|set|del", line):
+                            match = re.search("[0-9,]+(?=/)", line)
+                            if match:
+                                res.append(int(match.group().replace(',', '')))
+                print(res)
+                writer.writerow(res)
 
