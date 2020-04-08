e5cdc2451c0101f074ecf8c4da0256747a1edb1e
Daniel S. Berger <dsberger@cmu.edu>
2020-04-08 14:01:39 +0000

    simple python run script and output parser for cachebench

---
+++ cachelib/cachebench/test_configs/hit_ratio_test_configs/run.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import json
+import subprocess
+import re
+import io
+import csv
+
+config = {
+  "cache_config": {
+    "cacheSizeMB": 8192, 
+    "poolRebalanceIntervalSec": 0
+  }, 
+  "test_config": 
+    {
+      "addChainedRatio": 0.0, 
+      "delRatio": 0.0, 
+      "enableLookaside": True, 
+      "getRatio": 0.9746082749830369, 
+      "keySizeRange": [
+        1, 
+        8, 
+        64
+      ], 
+      "keySizeRangeProbability": [
+        0.3, 
+        0.7
+      ], 
+      "loneGetRatio": 0.025391725016963105, 
+      "numKeys": 23458726, 
+      "numOps": 5000000, 
+      "numThreads": 1,
+      "popDistFile": "assoc_altoona1_follower_pop.json", 
+      "prepopulateCache": True, 
+      "setRatio": 0.0, 
+      "valSizeDistFile": "assoc_altoona1_follower_sizes.json"
+    }
+ 
+}
+
+outfile = open('run.csv', 'w')
+writer = csv.writer(outfile)
+for threads in [1,4,8,16]:
+    config["test_config"]["numThreads"] = threads
+    config["test_config"]["numKeys"] = 100000
+    config["test_config"]["numOps"] = 1000
+    
+    
+    with open("run.json","w") as jfile:
+        json.dump(config, jfile, indent=2)
+    
+    #sudo /home/daniel/cachelib/build/bin/cachebench_bin --json_test_config ./fbobj_altoona2_follower_model_hr.json
+    
+    p = subprocess.Popen(["sudo","/home/daniel/cachelib/build/bin/cachebench_bin","--json_test_config","./run.json"], stdout=subprocess.PIPE)
+    
+    
+    exp = "[0-9,]+(?=/)"
+    
+    res = [config["test_config"]["numThreads"],
+        config["test_config"]["numKeys"],
+        config["test_config"]["numOps"]]
+
+    for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+        match = re.search(exp, line)
+        if match:
+            res.append(int(match.group().replace(',', '')))
+    print(res)
+    writer.writerow(res)
+
