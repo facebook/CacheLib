20a67841e6c09dac4376b06de6102a0a8e1983c1
dberger <dsberger@cmu.edu>
2020-04-12 11:11:21 -0400

    memcached comparison run script

--- cachelib/cachebench/test_configs/comparisontest/run.py
+++ cachelib/cachebench/test_configs/comparisontest/run.py
@@ -7,6 +7,12 @@ import io
 import csv
 import socket
 
+def parseRegex(regex, line):
+    match = re.search(regex, line)
+    if match:
+        return match.group().replace(',', '')
+
+
 config = {
   "cache_config": {
     "cacheSizeMB": 8192, 
@@ -29,7 +35,7 @@ config = {
       ], 
       "loneGetRatio": 0.025391725016963105, 
       "numKeys": 23458726,
-      "numOps": 5000000, 
+        "numOps": 20000,
       "numThreads": 1,
       "popDistFile": "assoc_altoona1_follower_pop.json", 
       "prepopulateCache": True, 
@@ -39,11 +45,15 @@ config = {
  
 }
 
+
+
 fn_log = "run_"+socket.gethostname()+".csv"
 fn_conf = "run_"+socket.gethostname()+".json"
-print(fn_log,fn_conf)
+err_log = "run_"+socket.gethostname()+"err.log"
+print("outputfiles:",fn_log,fn_conf,err_log)
 outfile = open(fn_log, 'w')
 writer = csv.writer(outfile)
+errfile = open(err_log, 'w')
 
 
 # need repetitions
@@ -51,15 +61,15 @@ writer = csv.writer(outfile)
 # different working set sizes
 
 for rep in range(0,10):
-    for sys in ["assoc","fbobj"]:
-        for num_keys in [2345872,23458726]:
-            for cache in ["cachelib","memcached"]:
-                for threads in [1,2,4,8,16,32,64]:
+    for num_keys in [23458726]: #2345872,
+        for threads in [32,64]: #1,2,4,8,16,64]:
+            for sys in ["assoc"]:  #,"fbobj"
+                for cache in ["memcached"]: #"cachelib"
                     config["test_config"]["popDistFile"] = sys+"_altoona1_follower_pop.json"
                     config["test_config"]["valSizeDistFile"] = sys+"_altoona1_follower_sizes.json"
                     config["test_config"]["numThreads"] = threads
                     config["test_config"]["numKeys"] = num_keys
-                    config["test_config"]["numOps"] = 300000000
+                    config["test_config"]["numOps"] = 250000000
                     if cache=="memcached":
                         config["test_config"]["mode"] = "micro"
                     else:
@@ -72,7 +82,11 @@ for rep in range(0,10):
                         config["test_config"]["numThreads"],
                         config["test_config"]["numKeys"],
                         config["test_config"]["numOps"],
-                        rep
+                        rep,
+                        -1, #6 hr
+                        -1, #7 gets
+                        -1, #8 sets
+                        -1  #9 dels
                     ]
                     
                     with open(fn_conf,"w") as jfile:
@@ -80,22 +94,44 @@ for rep in range(0,10):
                     
                     #sudo /home/daniel/cachelib/build/bin/cachebench_bin --json_test_config ./fbobj_altoona2_follower_model_hr.json
                     
-                    p = subprocess.Popen(["/proj/robinhood-PG0/cachelib/build/bin/cachebench_bin","--json_test_config","./"+fn_conf], stdout=subprocess.PIPE)
+                    p = subprocess.Popen(["/proj/robinhood-PG0/cachelib/build/bin/cachebench_bin","--json_test_config","./"+fn_conf], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 
                     warmup = True
+                    resultsout = ""
                     for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
                         if "Test Results" in line:
                             warmup = False
                         if not warmup:
-                            if "Hit Ratio" in line:
-                                match = re.search("[0-9.]+(?=%)", line)
-                                if match:
-                                    res.append(float(match.group().replace(',', '')))
-                            if re.search("get|set|del", line):
-                                match = re.search("[0-9,]+(?=/)", line)
-                                if match:
-                                    res.append(int(match.group().replace(',', '')))
+                            resultsout += line
+                            if "get" in line:
+                                # hr
+                                hr = parseRegex("[0-9.]+(?=%)", line)
+                                if hr:
+                                    res[6] = float(hr)
+                                # gets
+                                gets = parseRegex("[0-9,]+(?=/)", line)
+                                if gets:
+                                    res[7] = int(gets)
+                            if "set" in line:
+                                # sets
+                                sets = parseRegex("[0-9,]+(?=/)", line)
+                                if sets:
+                                    res[8] = int(sets)
+                            if "del" in line:
+                                # dels
+                                dels = parseRegex("[0-9,]+(?=/)", line)
+                                if dels:
+                                    res[9] = int(dels)
+
+                    if res[6]==-1 or res[7]==-1:
+                        # wasn't able to parse
+                        errfile.write(",".join(str(item) for item in res))
+                        errfile.write(resultsout)
+                        errfile.write("n".join(str(line) for line in p.stderr))
+                        errfile.flush()
+
                     print(res)
                     writer.writerow(res)
                     outfile.flush()
 outfile.close()
+errfile.close()
