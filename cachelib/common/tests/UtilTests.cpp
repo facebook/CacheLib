156c10a6a73722f6c9c562ce67597b2325bb2685
Sathya Gunasekar <sathya@fb.com>
2020-09-10 14:39:27 -0700

    reduce flakiness for util mem rss
    Summary:
    This test has been very flaky in estimating the process RSS after allocating
    some memory. Simplify the code and also add more logging to detect the state
    when this happens.
    
    Reviewed By: therealgymmy
    
    Differential Revision: D23619135
    
    fbshipit-source-id: 357edb4a163ddd95125c79ddb183f55ba3d77269

--- cachelib/common/tests/UtilTests.cpp
+++ cachelib/common/tests/UtilTests.cpp
@@ -160,7 +160,7 @@ TEST(Util, MemAvailable) { EXPECT_GT(util::getMemAvailable(), 0); }
 TEST(Util, MemRSS) {
   auto val = util::getRSSBytes();
   EXPECT_GT(val, 0);
-  size_t len = 500 * 1024 * 1024;
+  const size_t len = 500 * 1024 * 1024;
   void* ptr = ::mmap(nullptr, len, PROT_WRITE | PROT_READ,
                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   EXPECT_NE(MAP_FAILED, ptr);
@@ -168,8 +168,10 @@ TEST(Util, MemRSS) {
   std::memset(ptr, 5, len);
   // sleep to let the stat catch up.
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  EXPECT_GT(util::getRSSBytes(), val);
-  EXPECT_GE(util::getRSSBytes() - val, len);
+  auto newVal = util::getRSSBytes();
+  EXPECT_GT(newVal, val);
+  EXPECT_GE(newVal - val, len)
+      << folly::sformat("newVal= {}, val = {}, len = {}", newVal, val, len);
 }
 
 } // namespace tests
