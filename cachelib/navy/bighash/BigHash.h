061d6d20b3695b0295f48865d78a37a1735cc728
generatedunixname89002005325676 <generatedunixname89002005325676@fb.com>
2020-09-28 05:06:46 -0700

    Daily `arc lint --take CLANGFORMAT`
    Reviewed By: zertosh
    
    Differential Revision: D23959094
    
    fbshipit-source-id: 6caa046d263114bff38a38d756099aac357e4f04

--- cachelib/navy/bighash/BigHash.h
+++ cachelib/navy/bighash/BigHash.h
@@ -73,8 +73,7 @@ class BigHash final : public Engine {
 
   // Inserts key and value into BigHash. This will replace an existing
   // key if found. If it failed to write, it will return DeviceError.
-  Status insert(HashedKey hk,
-                BufferView value) override;
+  Status insert(HashedKey hk, BufferView value) override;
 
   // Removes an entry from BigHash if found. Ok on success, NotFound on miss,
   // and DeviceError on error.
