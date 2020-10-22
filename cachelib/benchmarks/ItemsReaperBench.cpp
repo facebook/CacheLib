46ce4a459c9690c378298d0f2f664a1a7a3535e9
Sathya Gunasekar <sathya@fb.com>
2020-10-21 22:50:47 -0700

    update reaper benchmark to measure cpu util
    Summary:
    This benchmark was originally written to measure the speeed of hashtable iterator based
    reaper. Repurposing this to measure the throughput of slab walk reaper and
    also measuring the cpu overhead.
    
    Reviewed By: therealgymmy
    
    Differential Revision: D24156510
    
    fbshipit-source-id: bb6231f87419755b723f15e47d2ac61bd07c24c3

--- cachelib/benchmarks/ItemsReaperBench.cpp
+++ cachelib/benchmarks/ItemsReaperBench.cpp
@@ -1,32 +1,27 @@
 #include <iomanip>
 
+#include <sys/resource.h>
+#include <sys/time.h>
+
 #include <folly/Benchmark.h>
 #include <folly/Random.h>
 #include <folly/init/Init.h>
+#include <folly/lang/Bits.h>
+#include <folly/logging/xlog.h>
 
 #include "cachelib/allocator/CacheAllocator.h"
+#include "cachelib/common/TestUtils.h"
 
 using namespace facebook::cachelib;
 
 DEFINE_uint64(num_keys, 1 << 20, "Number of keys to populate the cache with");
-DEFINE_uint64(hash_power,
-              21,
-              "Hash power controlling the number of buckets in hash table");
-DEFINE_uint64(lock_power,
-              10,
-              "Lock power controlling number of hash table locks");
-DEFINE_uint64(size_bytes,
-              1024 * 10 * Slab::kSize,
-              "Size of the cache in bytes");
-DEFINE_uint32(reaper_interval_s,
-              1,
-              "time to sleep between each reaping period");
-DEFINE_uint32(reaper_sleep_time_ms, 10, "reaper throttler sleep time");
-DEFINE_uint32(reaper_sleep_interval_ms, 5, "reaper throttler sleep interval");
-DEFINE_uint32(print_interval_ms,
+DEFINE_uint64(size_gb, 10, "Size of the cache in gb");
+DEFINE_uint32(sleep_ms, 10, "reaper throttler sleep time");
+DEFINE_uint32(work_ms, 5, "reaper throttler work interval");
+DEFINE_uint32(print_interval_s,
               100,
-              "interval at which to print reaper speed");
-DEFINE_uint32(benchmark_duration_s, 10, "how long to run the benchmark");
+              "interval at which to print reaper speed. 0 means disabled");
+DEFINE_uint32(benchmark_duration_s, 300, "how long to run the benchmark");
 
 namespace {
 
@@ -47,73 +42,102 @@ static uint32_t getRandomAllocSize(double val) {
   }
 }
 
-std::string generateKey(std::mt19937& gen) {
-  std::uniform_int_distribution<uint32_t> keySize(kMinKeySize, kMaxKeySize);
-  std::uniform_int_distribution<uint8_t> charDis(0, 25);
-  const auto keyLen = keySize(gen);
-  std::string s;
-  for (unsigned int j = 0; j < keyLen; j++) {
-    s += ('a' + charDis(gen));
-  }
-  return s;
-}
-
 } // namespace
 
 int main(int argc, char** argv) {
   folly::init(&argc, &argv);
 
-  std::cout << "initializing cache" << std::endl;
-  LruAllocator::AccessConfig accessConfig(FLAGS_hash_power, FLAGS_lock_power);
+  XLOG(INFO) << "initializing cache";
+  LruAllocator::AccessConfig accessConfig(
+      folly::findLastSet(FLAGS_num_keys) + 1, 10);
   LruAllocator::Config lruConfig;
-  lruConfig.setCacheSize(FLAGS_size_bytes);
+  lruConfig.setCacheSize(FLAGS_size_gb * 1024 * 1024 * 1024);
   lruConfig.setAccessConfig(accessConfig);
   lruConfig.enableItemReaperInBackground(
-      std::chrono::milliseconds{FLAGS_reaper_interval_s},
-      util::Throttler::Config{FLAGS_reaper_sleep_time_ms,
-                              FLAGS_reaper_sleep_interval_ms});
+      std::chrono::milliseconds{FLAGS_sleep_ms},
+      util::Throttler::Config{FLAGS_sleep_ms, FLAGS_work_ms});
   assert(lruConfig.itemsReaperEnabled());
   LruAllocator cache(lruConfig);
   const auto poolId =
       cache.addPool("default", cache.getCacheMemoryStats().cacheSize);
 
-  std::cout << "allocating items" << std::endl;
+  XLOG(INFO) << "allocating items";
   std::mt19937 gen(folly::Random::rand32());
   std::normal_distribution<> sizeDis(kAllocSizeMean, kAllocSizeDev);
+  std::uniform_int_distribution<uint32_t> keySize(kMinKeySize, kMaxKeySize);
+  std::uniform_int_distribution<uint32_t> ttlDist(0,
+                                                  FLAGS_benchmark_duration_s);
+  std::uniform_int_distribution<uint8_t> charDis(0, 25);
   for (uint64_t i = 0; i < FLAGS_num_keys; ++i) {
-    util::allocateAccessible(cache, poolId, generateKey(gen),
-                             getRandomAllocSize(sizeDis(gen)),
-                             /*ttlSecs=*/36000);
+    util::allocateAccessible(
+        cache, poolId,
+        facebook::cachelib::test_util::getRandomAsciiStr(keySize(gen)),
+        getRandomAllocSize(sizeDis(gen)), ttlDist(gen));
   }
 
-  std::cout << "benchmarking" << std::endl;
-  const auto start = std::chrono::steady_clock::now();
-  auto lastPrint = start;
-  std::chrono::steady_clock::time_point now;
-  const auto initialVisited = cache.getReaperStats().numVisitedItems;
-  uint64_t lastVisited = initialVisited;
-  do {
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds(FLAGS_print_interval_ms));
-    const auto curVisited = cache.getReaperStats().numVisitedItems;
-    now = std::chrono::steady_clock::now();
-    const auto timeSpent =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(now - lastPrint);
-    const auto visited = curVisited - lastVisited;
-    std::cout << "visited " << visited << " in " << timeSpent.count() << " ns. "
-              << std::fixed << std::setprecision(2)
-              << visited / (timeSpent.count() * 0.000000001) << " visits/sec"
-              << std::endl;
-    lastVisited = curVisited;
-    lastPrint = now;
-  } while (now - start < std::chrono::seconds(FLAGS_benchmark_duration_s));
+  auto reaperStatStr = [](const facebook::cachelib::ReaperStats& stats) {
+    auto str = folly::sformat(
+        "numTraversals: {:8d}, numVisits: {:12d}, lastTraversalMs: {:6d}ms, "
+        "avgTraversalMs: {:6d}ms, maxTraversalMs: {:6d}",
+        stats.numTraversals, stats.numVisitedItems, stats.lastTraversalTimeMs,
+        stats.avgTraversalTimeMs, stats.maxTraversalTimeMs);
+    return str;
+  };
 
-  const auto timeSpent =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(now - start);
-  std::cout << "overall visit speed: " << std::fixed << std::setprecision(2)
-            << (cache.getReaperStats().numVisitedItems - initialVisited) /
-                   (timeSpent.count() * 0.000000001)
-            << std::endl;
+  struct rusage rUsageBefore = {};
+  struct rusage rUsageAfter = {};
+
+  XLOG(INFO) << "Gathering reaper metrics";
+  if (::getrusage(RUSAGE_SELF, &rUsageBefore)) {
+    XLOG(INFO) << "Error getting rusage" << errno;
+    return 0;
+  }
+  const auto start = std::chrono::steady_clock::now();
+  const auto startStats = cache.getReaperStats();
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    if (FLAGS_print_interval_s) {
+      XLOG_EVERY_MS(INFO, FLAGS_print_interval_s * 1000)
+          << reaperStatStr(cache.getReaperStats());
+    }
+
+    if (std::chrono::steady_clock::now() - start >
+        std::chrono::seconds(FLAGS_benchmark_duration_s)) {
+      break;
+    }
+  }
+
+  if (::getrusage(RUSAGE_SELF, &rUsageAfter)) {
+    XLOG(INFO) << "Error getting rusage" << errno;
+    return 0;
+  }
+
+  auto finalStats = cache.getReaperStats();
+  double memoryScanned =
+      FLAGS_size_gb * (finalStats.numTraversals - startStats.numTraversals);
+  auto timeTaken = std::chrono::duration_cast<std::chrono::seconds>(
+      std::chrono::steady_clock::now() - start);
+
+  XLOG(INFO) << reaperStatStr(finalStats);
+  XLOG(INFO) << folly::sformat("bytes scanned  : {:12.2f}gb/sec",
+                               memoryScanned / timeTaken.count());
+
+  auto getTimeRUsage = [](const struct rusage& r) {
+    double userSeconds = r.ru_utime.tv_sec + r.ru_utime.tv_usec / 1e6;
+    double sysSeconds = r.ru_stime.tv_sec + r.ru_stime.tv_usec / 1e6;
+    return std::make_tuple(userSeconds, sysSeconds, userSeconds + sysSeconds);
+  };
+
+  auto [beforeUser, beforeSys, beforeTot] = getTimeRUsage(rUsageBefore);
+  auto [afterUser, afterSys, afterTot] = getTimeRUsage(rUsageAfter);
+
+  // compute core cpu util by divinding the time spent on a core with overall
+  // time spent to complete the operation.
+  XLOG(INFO) << folly::sformat(
+      "cpu util: user: {:3.6}s sys: {:3.6f}s total: {:3.6f}s  util-pct: "
+      "{:2.2f}%",
+      afterUser - beforeUser, afterSys - beforeSys, afterTot - beforeTot,
+      100.0 * (afterTot - beforeTot) / timeTaken.count());
 
   return 0;
 }
