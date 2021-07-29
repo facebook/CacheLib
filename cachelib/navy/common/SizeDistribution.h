fff79287bed27523f4ce6037974fd62bd1115c0b
Jimmy Lu <beyondsora@fb.com>
2021-07-28 20:36:02 -0700

    Document SizeDistribution.h
    Summary: as title
    
    Reviewed By: jiayuebao
    
    Differential Revision: D29950685
    
    fbshipit-source-id: 12d41666dc8de83236c23eb8718b3be60e5088d4

--- cachelib/navy/common/SizeDistribution.h
+++ cachelib/navy/common/SizeDistribution.h
@@ -12,6 +12,8 @@
 namespace facebook {
 namespace cachelib {
 namespace navy {
+// This is used to track distribution of a set of sizes. This class supports
+// both adding and removing sizes from the distribution.
 class SizeDistribution {
  public:
   // Create a size distribution that spans [@min, @max] at a granularity of
@@ -21,13 +23,19 @@ class SizeDistribution {
   // Recover from a previously saved snapshot
   explicit SizeDistribution(std::map<int64_t, int64_t> snapshot);
 
+  // Add a new sample of "size" to the distribution
+  // @size    size should be between [min, max] specified at construction
   void addSize(uint64_t size);
+
+  // Remove a sample of "size" to the distribution
+  // @size    size should be between [min, max] specified at construction
   void removeSize(uint64_t size);
 
   // Return {size -> number of items} mapping
   // Return signed value so it's easy to use this with thrift structures
   std::map<int64_t, int64_t> getSnapshot() const;
 
+  // Clear all samples
   void reset();
 
  private:
