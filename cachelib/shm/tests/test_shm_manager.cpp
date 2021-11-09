315659254f31e54ec37a389189cf12b3686cda42
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2021-11-08 19:46:04 -0500

    Fix test_shm_manager.cpp test
    It wrongly assumed that the only possible segment type is
    PosixSysV segment.

--- cachelib/shm/tests/test_shm_manager.cpp
+++ cachelib/shm/tests/test_shm_manager.cpp
@@ -797,8 +797,8 @@ void ShmManagerTest::testShutDown(bool posix) {
     ASSERT_NO_THROW(s.createShm(seg2, seg2Size, nullptr, seg2Opt));
     ASSERT_EQ(s.getShmByName(seg2).getSize(), seg2Size);
     auto *v = std::get_if<PosixSysVSegmentOpts>(&s.getShmTypeByName(seg2));
-    ASSERT_TRUE(v);
-    ASSERT_EQ(v->usePosix, posix);
+    if (v)
+      ASSERT_EQ(v->usePosix, posix);
 
     ASSERT_TRUE(s.shutDown() == ShutDownRes::kSuccess);
   };
