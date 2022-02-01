e3a0362f097cba6add204f2b35ae6e1e2a1b8a83
Assaf Gordon <assafgordon@fb.com>
2022-02-01 09:34:57 -0800

    portability: #define MADV_DODUMP if missing
    Summary:
    On some systems (e.g. macOS), some madvise(2) flags are
    not defined - define them as zero.
    
    Reviewed By: therealgymmy
    
    Differential Revision: D33741807
    
    fbshipit-source-id: 1e6946e74677044c386e68d9ad6eb8b6e47c65c0

--- cachelib/allocator/TlsActiveItemRing.h
+++ cachelib/allocator/TlsActiveItemRing.h
@@ -24,6 +24,11 @@
 namespace facebook {
 namespace cachelib {
 
+/* Missing madvise(2) flag on Mac OS */
+#ifndef MADV_DODUMP
+#define MADV_DODUMP 0
+#endif
+
 // Thread local class for tracking recently accessed item.
 class TlsActiveItemRing {
   static const size_t KB = 1024ULL;
