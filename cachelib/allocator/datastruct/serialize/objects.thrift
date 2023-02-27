6ad7a318b43de25ba41067c3d6a851d5a60d1633
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2023-02-27 10:31:09 -0800

    fbcode/cachelib/allocator/datastruct/serialize
    Reviewed By: avalonalex
    
    Differential Revision: D43616310
    
    fbshipit-source-id: 3367ad01ba31e5dc561d63a4f3b9746170e64912

--- cachelib/allocator/datastruct/serialize/objects.thrift
+++ cachelib/allocator/datastruct/serialize/objects.thrift
@@ -22,17 +22,17 @@ namespace cpp2 facebook.cachelib.serialization
 
 // Saved state for an SList
 struct SListObject {
-  2: required i64 size,
-  3: required i64 compressedHead, // Pointer to the head element
+  2: required i64 size;
+  3: required i64 compressedHead; // Pointer to the head element
   // TODO(bwatling): remove the default value and clean up SList::SList() once
   // we can rely on 'compressedTail' always being valid.
-  4: i64 compressedTail = -1, // Pointer to the tail element
+  4: i64 compressedTail = -1; // Pointer to the tail element
 }
 
 struct DListObject {
-  1: required i64 compressedHead,
-  2: required i64 compressedTail,
-  3: required i64 size,
+  1: required i64 compressedHead;
+  2: required i64 compressedTail;
+  3: required i64 size;
 }
 
 struct MultiDListObject {
