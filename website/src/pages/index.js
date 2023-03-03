496ed376b7000bca496e03a2b1cb4e47d24e7f24
Jaesoo Lee <jaesoo@meta.com>
2023-03-03 12:13:05 -0800

    fix another broken installation page link in index.js
    Summary: There is another missing link in index.js for installation page. This change fixes it.
    
    Reviewed By: jiayuebao
    
    Differential Revision: D43774697
    
    fbshipit-source-id: 1c2ff1c801ae41ea6b50e6e687714f422bcb0c92

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -117,7 +117,7 @@ function Home() {
                 'button button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={ useBaseUrl('docs/installation/installation') }>
+              to={ useBaseUrl('docs/installation') }>
               Get Started
             </Link>
     </div>
