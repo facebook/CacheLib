ffa54709fb021fa2d15494fed39d60df53b0fb0f
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-08 16:09:11 -0800

    fbcode/cachelib/allocator/datastruct/tests
    Differential Revision: D43869054
    
    fbshipit-source-id: e90b16576404b8eb5819c65d61fe972f0f518c66

--- cachelib/allocator/datastruct/tests/test_objects.thrift
+++ cachelib/allocator/datastruct/tests/test_objects.thrift
@@ -20,6 +20,6 @@ namespace cpp2 facebook.cachelib.test_serialization
 // testing warm rolls from the old format to the new format.
 // TODO(bwatling): remove this when 'compressedTail' is always present.
 struct SListObjectNoCompressedTail {
-  2: required i64 size,
-  3: required i64 compressedHead, // Pointer to the head element
+  2: required i64 size;
+  3: required i64 compressedHead; // Pointer to the head element
 }
