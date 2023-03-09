1031b13fc914104ca9b606edc7c183896e0bb46a
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-08 16:13:55 -0800

    fbcode/cachelib/experimental/objcache2/tests
    Differential Revision: D43869046
    
    fbshipit-source-id: 288bc98b8146424f71ece5d0685d08c59e3ed523

--- cachelib/experimental/objcache2/tests/test_object.thrift
+++ cachelib/experimental/objcache2/tests/test_object.thrift
@@ -17,13 +17,13 @@
 namespace cpp2 facebook.cachelib.objcache2.test
 
 struct ThriftFoo {
-    1: i32 a;
-    2: i32 b;
-    3: i32 c;
+  1: i32 a;
+  2: i32 b;
+  3: i32 c;
 }
 
 struct ThriftFoo2 {
-    1: i32 d;
-    2: i32 e;
-    3: i32 f;
+  1: i32 d;
+  2: i32 e;
+  3: i32 f;
 }
