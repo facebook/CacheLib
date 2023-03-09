0bdb93505ef6cb86ea0950a3889ee2dacf066cb0
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-09 08:10:36 -0800

    fbcode/cachelib/allocator/serialize
    Differential Revision: D43869057
    
    fbshipit-source-id: 84aa3aec8d75f04b640d6aa8393002190c97842f

--- cachelib/allocator/serialize/objects.thrift
+++ cachelib/allocator/serialize/objects.thrift
@@ -23,136 +23,136 @@ include "cachelib/allocator/datastruct/serialize/objects.thrift"
 // make sure to communicate that with our users.
 
 struct CacheAllocatorMetadata {
-  1: required i64 allocatorVersion, // version of cache alloctor
-  2: i64 cacheCreationTime = 0, // time when the cache was created.
-  3: required i64 accessType = 0, // default chained alloc
-  4: required i64 mmType = 0, // default LRU
-  5: map<byte, map<byte, i64>> fragmentationSize,
-  6: list<byte> compactCachePools,
-  7: i64 numPermanentItems,
-  8: i64 numChainedParentItems,
-  9: i64 numChainedChildItems,
-  10: i64 ramFormatVersion = 0, // format version of ram cache
-  11: i64 numAbortedSlabReleases = 0, // number of times slab release is aborted
+  1: required i64 allocatorVersion; // version of cache alloctor
+  2: i64 cacheCreationTime = 0; // time when the cache was created.
+  3: required i64 accessType = 0; // default chained alloc
+  4: required i64 mmType = 0; // default LRU
+  5: map<byte, map<byte, i64>> fragmentationSize;
+  6: list<byte> compactCachePools;
+  7: i64 numPermanentItems;
+  8: i64 numChainedParentItems;
+  9: i64 numChainedChildItems;
+  10: i64 ramFormatVersion = 0; // format version of ram cache
+  11: i64 numAbortedSlabReleases = 0; // number of times slab release is aborted
 }
 
 struct NvmCacheMetadata {
-  1: i64 nvmFormatVersion = 0,
-  2: i64 creationTime = 0,
-  3: bool safeShutDown = false,
-  4: bool encryptionEnabled = false,
-  5: bool truncateAllocSize = false,
+  1: i64 nvmFormatVersion = 0;
+  2: i64 creationTime = 0;
+  3: bool safeShutDown = false;
+  4: bool encryptionEnabled = false;
+  5: bool truncateAllocSize = false;
 }
 
 struct CompactCacheMetadataObject {
-  1: required i64 keySize,
-  2: required i64 valueSize,
+  1: required i64 keySize;
+  2: required i64 valueSize;
 }
 
 struct CompactCacheAllocatorObject {
-  1: required list<i64> chunks,
-  2: required CompactCacheMetadataObject ccMetadata,
+  1: required list<i64> chunks;
+  2: required CompactCacheMetadataObject ccMetadata;
 }
 
 struct CompactCacheAllocatorManagerObject {
-  1: required map<string, CompactCacheAllocatorObject> allocators,
+  1: required map<string, CompactCacheAllocatorObject> allocators;
 }
 
 struct MMLruConfig {
-  1: required i32 lruRefreshTime,
-  2: required bool updateOnWrite,
-  3: required i32 lruInsertionPointSpec,
-  4: bool updateOnRead = true,
-  5: bool tryLockUpdate = false,
-  6: double lruRefreshRatio = 0.0,
+  1: required i32 lruRefreshTime;
+  2: required bool updateOnWrite;
+  3: required i32 lruInsertionPointSpec;
+  4: bool updateOnRead = true;
+  5: bool tryLockUpdate = false;
+  6: double lruRefreshRatio = 0.0;
 }
 
 struct MMLruObject {
-  1: required MMLruConfig config,
+  1: required MMLruConfig config;
 
   // number of evictions for this MM object.
-  5: i64 evictions = 0,
+  5: i64 evictions = 0;
 
-  6: required i64 insertionPoint,
-  7: required i64 tailSize,
-  8: required DListObject lru,
-  9: required i64 compressedInsertionPoint,
+  6: required i64 insertionPoint;
+  7: required i64 tailSize;
+  8: required DListObject lru;
+  9: required i64 compressedInsertionPoint;
 }
 
 struct MMLruCollection {
-  1: required map<i32, map<i32, MMLruObject>> pools,
+  1: required map<i32, map<i32, MMLruObject>> pools;
 }
 
 struct MM2QConfig {
-  1: required i32 lruRefreshTime,
-  2: required bool updateOnWrite,
-  3: required i32 hotSizePercent,
-  4: required i32 coldSizePercent,
-  5: bool updateOnRead = true,
-  6: bool tryLockUpdate = false,
-  7: bool rebalanceOnRecordAccess = true,
-  8: double lruRefreshRatio = 0.0,
+  1: required i32 lruRefreshTime;
+  2: required bool updateOnWrite;
+  3: required i32 hotSizePercent;
+  4: required i32 coldSizePercent;
+  5: bool updateOnRead = true;
+  6: bool tryLockUpdate = false;
+  7: bool rebalanceOnRecordAccess = true;
+  8: double lruRefreshRatio = 0.0;
 }
 
 struct MM2QObject {
-  1: required MM2QConfig config,
-  13: bool tailTrackingEnabled = false,
+  1: required MM2QConfig config;
+  13: bool tailTrackingEnabled = false;
 
   // number of evictions for this MM object.
-  11: i64 evictions = 0,
+  11: i64 evictions = 0;
 
   // Warm, hot and cold lrus
-  12: required MultiDListObject lrus,
+  12: required MultiDListObject lrus;
 }
 
 struct MM2QCollection {
-  1: required map<i32, map<i32, MM2QObject>> pools,
+  1: required map<i32, map<i32, MM2QObject>> pools;
 }
 
 struct MMTinyLFUConfig {
-  1: required i32 lruRefreshTime,
-  2: required bool updateOnWrite,
-  3: required i32 windowToCacheSizeRatio,
-  4: required i32 tinySizePercent,
-  5: bool updateOnRead = true,
-  6: bool tryLockUpdate = false,
-  7: double lruRefreshRatio = 0.0,
+  1: required i32 lruRefreshTime;
+  2: required bool updateOnWrite;
+  3: required i32 windowToCacheSizeRatio;
+  4: required i32 tinySizePercent;
+  5: bool updateOnRead = true;
+  6: bool tryLockUpdate = false;
+  7: double lruRefreshRatio = 0.0;
 }
 
 struct MMTinyLFUObject {
-  1: required MMTinyLFUConfig config,
+  1: required MMTinyLFUConfig config;
 
   // number of evictions for this MM object.
-  2: i64 evictions = 0,
+  2: i64 evictions = 0;
 
   // Warm, hot and cold lrus
-  3: required MultiDListObject lrus,
+  3: required MultiDListObject lrus;
 }
 
 struct MMTinyLFUCollection {
-  1: required map<i32, map<i32, MMTinyLFUObject>> pools,
+  1: required map<i32, map<i32, MMTinyLFUObject>> pools;
 }
 
 struct ChainedHashTableObject {
   // fields in ChainedHashTable::Config
-  1: required i32 bucketsPower,
-  2: required i32 locksPower,
-  3: i64 numKeys,
+  1: required i32 bucketsPower;
+  2: required i32 locksPower;
+  3: i64 numKeys;
 
   // this magic id ensures on a warm roll, user cannot
   // start the cache with a different hash function
-  4: i32 hasherMagicId = 0,
+  4: i32 hasherMagicId = 0;
 }
 
 struct MMTTLBucketObject {
-  4: i64 expirationTime,
-  5: i64 creationTime,
-  6: required DListObject dList,
+  4: i64 expirationTime;
+  5: i64 creationTime;
+  6: required DListObject dList;
 }
 
 struct TTLBucketCollection {
-  1: required map<i64, MMTTLBucketObject> buckets,
-  2: i64 minEpoch = 0,
-  3: i64 maxTTL = 0,
-  4: i64 interval = 0,
+  1: required map<i64, MMTTLBucketObject> buckets;
+  2: i64 minEpoch = 0;
+  3: i64 maxTTL = 0;
+  4: i64 interval = 0;
 }
