872d61a6f09ce100c510069f526ce9fb9cb701c6
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-08 16:08:53 -0800

    fbcode/cachelib/benchmarks
    Differential Revision: D43869116
    
    fbshipit-source-id: c62c2518691313ec82c15c9f65ac304378222257

--- cachelib/benchmarks/DataTypeBench.thrift
+++ cachelib/benchmarks/DataTypeBench.thrift
@@ -17,11 +17,9 @@
 namespace cpp2 facebook.cachelib.datatypebench
 
 struct StdMap {
-  1: required map<i32, string> m,
+  1: required map<i32, string> m;
 }
 
 struct StdUnorderedMap {
-  1: required map<i32, string>
-  (cpp.template = "std::unordered_map")
-  m,
+  1: required map<i32, string> (cpp.template = "std::unordered_map") m;
 }
