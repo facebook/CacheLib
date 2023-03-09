99a05d35a3ed9a782262160e9e2119b90a41f86e
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-09 08:16:02 -0800

    fbcode/cachelib/experimental/objcache
    Differential Revision: D43869089
    
    fbshipit-source-id: 57d359fa8cfeaca395f636f6d3997a934afca94a

--- cachelib/experimental/objcache/ObjectCachePersistence.thrift
+++ cachelib/experimental/objcache/ObjectCachePersistence.thrift
@@ -17,9 +17,9 @@
 namespace cpp2 facebook.cachelib.objcache.serialization
 
 struct Item {
-  1: byte poolId
-  2: i32 creationTime,
-  3: i32 expiryTime,
-  4: string key,
-  5: string payload,
+  1: byte poolId;
+  2: i32 creationTime;
+  3: i32 expiryTime;
+  4: string key;
+  5: string payload;
 }
