d92910bc096906f94c0b9ef888e1b2d7fcdac68d
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-03-08 16:14:25 -0800

    fbcode/cachelib/experimental/objcache/tests
    Differential Revision: D43869073
    
    fbshipit-source-id: 52fea96db9cefe735ba56b564bf6af5566cd6d76

--- cachelib/experimental/objcache/tests/ThriftCustomAllocator.thrift
+++ cachelib/experimental/objcache/tests/ThriftCustomAllocator.thrift
@@ -24,22 +24,26 @@ struct UseSimpleCustomAllocator {
 
   // A template type like map needs to use "cpp.template" to specify a replacement template
   1: map<
+    // A concrete type like string needs to use "cpp.type" to specify a replacement type
+    string (
+      cpp.use_allocator,
+      cpp.type = "facebook::cachelib::objcache::test::TestString",
+    ),
+    string (
+      cpp.use_allocator,
+      cpp.type = "facebook::cachelib::objcache::test::TestString",
+    )
+  > (
+    cpp.use_allocator,
+    cpp.template = "facebook::cachelib::objcache::test::TestMap",
+  ) m;
 
-      // A concrete type like string needs to use "cpp.type" to specify a replacement type
-      string
-        (cpp.use_allocator,
-         cpp.type = "facebook::cachelib::objcache::test::TestString"),
-
-      string
-        (cpp.use_allocator,
-         cpp.type = "facebook::cachelib::objcache::test::TestString")
-
-     > (cpp.use_allocator,
-        cpp.template = "facebook::cachelib::objcache::test::TestMap") m;
-
-   // Native types or types that do not allocate memory do NOT need custom allocator
-   2: i32 m2;
-} (cpp.allocator="facebook::cachelib::objcache::test::ScopedTestAllocator", cpp.allocator_via="m")
+  // Native types or types that do not allocate memory do NOT need custom allocator
+  2: i32 m2;
+} (
+  cpp.allocator = "facebook::cachelib::objcache::test::ScopedTestAllocator",
+  cpp.allocator_via = "m",
+)
 // TODO: thrift allocator propagation behavior is broken. Right now, for the following
 //          myObj1 = myObj2;
 //       even if the allocator copy-assignment propagation is false, myObj2's
@@ -50,17 +54,21 @@ struct UseSimpleCustomAllocator {
 
 union UnionWithCustomAllocator {
   1: map<
-      i32,
-      string
-        (cpp.use_allocator,
-         cpp.type = "facebook::cachelib::objcache::test::TestString")
-     > (cpp.use_allocator,
-        cpp.template = "facebook::cachelib::objcache::test::TestMap")m1;
-  2: string
-      (cpp.use_allocator,
-       cpp.type = "facebook::cachelib::objcache::test::TestString") m2;
+    i32,
+    string (
+      cpp.use_allocator,
+      cpp.type = "facebook::cachelib::objcache::test::TestString",
+    )
+  > (
+    cpp.use_allocator,
+    cpp.template = "facebook::cachelib::objcache::test::TestMap",
+  ) m1;
+  2: string (
+    cpp.use_allocator,
+    cpp.type = "facebook::cachelib::objcache::test::TestString",
+  ) m2;
   3: i32 m3;
-} (cpp.allocator="facebook::cachelib::objcache::test::ScopedTestAllocator")
+} (cpp.allocator = "facebook::cachelib::objcache::test::ScopedTestAllocator")
 // TODO: even though thrift union does not support allocator. We still need to
 //       annotate it with allocator so it has a `get_allocator()` method so
 //       that when deserializing it will be able to pass an allocator an inner
@@ -89,11 +97,14 @@ union UnionWithCustomAllocator {
 //          }
 
 struct UseTwoF14Maps {
-  1: map<i32, i32>
-       (cpp.use_allocator,
-        cpp.template = "facebook::cachelib::objcache::test::TestFollyF14FastMap") m1;
-  2: map<i32, double>
-       (cpp.use_allocator,
-        cpp.template = "facebook::cachelib::objcache::test::TestFollyF14FastMap") m2;
-} (cpp.allocator=
-     "facebook::cachelib::objcache::test::TestF14TemplateAllocator<std::pair<const int32_t, int32_t>>")
+  1: map<i32, i32> (
+    cpp.use_allocator,
+    cpp.template = "facebook::cachelib::objcache::test::TestFollyF14FastMap",
+  ) m1;
+  2: map<i32, double> (
+    cpp.use_allocator,
+    cpp.template = "facebook::cachelib::objcache::test::TestFollyF14FastMap",
+  ) m2;
+} (
+  cpp.allocator = "facebook::cachelib::objcache::test::TestF14TemplateAllocator<std::pair<const int32_t, int32_t>>",
+)
