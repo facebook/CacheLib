cc713504bbeb5f2e821859ba78b7917dc28896a0
Richard Barnes <rbarnes@meta.com>
2024-02-25 22:27:52 -0800

    Fix deprecated behaviour in cachelib/common/PercentileStats.cpp
    Summary:
    Future C++ standards and compiler upgrades will eliminate deprecated behaviour. `-Wdeprecated` identifies this behaviour and has found some in this code!
    
    Some examples.
    
    **Dynamic exceptions**
    ```
    error: dynamic exception specifications are deprecated [-Werror,-Wdeprecated-dynamic-exception-spec]
    ```
    `throw(...)` has been deprecated since C++11 and removed in C++17. In most cases we can just use `noexcept` in the rest, we can remove this.
    
    **Implicit copy constructors**
    ```
    error: definition of implicit copy constructor for 'XXX' is deprecated because it has a user-declared destructor [-Werror,-Wdeprecated-copy-with-dtor]
    ```
    If you define a destructor, you need to explicitly define a copy constructor.
    
    **Out-ofline constexpr static**
    ```
    error: out-of-line definition of constexpr static data member is redundant in C++17 and is deprecated [-Werror,-Wdeprecated]
    ```
    This can be simplified:
    ```
    class MyClass {
        static constexpr my_const = 3;
    };
    
    static constexpr MyClass::my_const; // <- No longer needed!
    ```
    
    Reviewed By: meyering
    
    Differential Revision: D54158205
    
    fbshipit-source-id: c6909e9542bdd36a096768013e791b4763975b6e

--- cachelib/common/PercentileStats.cpp
+++ cachelib/common/PercentileStats.cpp
@@ -19,10 +19,9 @@
 namespace facebook {
 namespace cachelib {
 namespace util {
-const std::array<double, 14> PercentileStats::kQuantiles{
+constexpr std::array<double, 14> PercentileStats::kQuantiles{
     0,    0.05, 0.1,   0.25,   0.5,     0.75,     0.9,
     0.95, 0.99, 0.999, 0.9999, 0.99999, 0.999999, 1.0};
-constexpr int PercentileStats::kDefaultWindowSize;
 
 PercentileStats::Estimates PercentileStats::estimate() {
   estimator_.flush();
